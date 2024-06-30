e7cd995c3598f5bea2ef661d06bae6a25d871448
omarramadan021 <omarramadan021@gmail.com>
2024-06-30 21:15:32 +0000

    added homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,31 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:firetest/auth/login.dart';
+import 'package:firetest/auth/signup.dart';
 import 'package:flutter/material.dart';
-import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      home: Login(),
+      routes: {
+        "signup" : (context) => SignUp() , 
+        "login" : (context) => Login()
+      },
     );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
