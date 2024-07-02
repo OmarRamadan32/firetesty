c57003716c7bd9fa13ca77b0697c20c8b45bfdbd
omarramadan021 <omarramadan021@gmail.com>
2024-07-02 21:52:24 +0000

    nothing

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,16 +1,18 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
-
-
 
   @override
   State<Homepage> createState() => _HomepageState();
 }
 
 class _HomepageState extends State<Homepage> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,10 +20,6 @@ class _HomepageState extends State<Homepage> {
           title: const Text('Firebase Install'),
           actions: [],
         ),
-        body: ListView(
-          children: [
-            // Text("How Are You", style: )
-          ],
-        ));
+        body: Text("Heloooooooooooooooooooo"));
   }
 }
